9bfcd3e8d2d963f96799af7aa348388b1bc51569
Vinayrajgambo <Vinayraj.gambo@gmail.com>
2023-10-30 22:39:01 +0530

    hhh

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ conda create --prefix ./env python=3.8 -y
 
 echo [$(date)]: "activating the environment"
 
-source activate ./env
+source activate ./venv
 
 echo [$(date)]: "installing the dev requirements"
 
