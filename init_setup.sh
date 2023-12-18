5922f728b5d32f9412467a0875f712e545338dab
Vinayrajgambo <134421737+Vinayrajgambo@users.noreply.github.com>
2023-12-18 21:34:41 +0530

    Update init_setup.sh
    env change

--- init_setup.sh
+++ init_setup.sh
@@ -8,7 +8,7 @@ conda create --prefix ./env python=3.8 -y
 
 echo [$(date)]: "activating the environment"
 
-source activate ./venv
+source activate ./env
 
 echo [$(date)]: "installing the dev requirements"
 
